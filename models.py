d5e948c3460a7ec977292e82e1b4e376d9ac9676
Larraine <larrainenataliacamposano@gmail.com>
2025-10-26 11:25:52 +0800

    Update models.py

--- models.py
+++ models.py
@@ -2,14 +2,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Record:
+class Record(db.Model):
     __tablename__ = 'records'
-    
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     course = db.Column(db.String(120), nullable=False)
     year = db.Column(db.Integer, nullable=False)
-program = db.Column(db.String(100), nullable=False)
+    program = db.Column(db.String(100), nullable=False)
 
     def __repr__(self):
-        return Record {self.name}>"
+        return f"<Record {self.name}>"
